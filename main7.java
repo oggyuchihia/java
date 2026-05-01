20eb237ac3f6ef92616ea0033abdfc85221d3e04
oggyuchihia <bhishmmori@gmail.com>
2026-05-01 18:40:43 +0530

    control structures 2

---
+++ main7.java
@@ -0,0 +1,18 @@
+class main7{
+    public static void main(String[] args)
+    {
+        boolean sunny=true;
+        boolean hot=false;
+        if (sunny){
+            if (hot){
+                System.out.println("cap and half sleeves");
+            }
+            else{
+                System.out.println("half sleeves");
+            }
+        }
+        else{
+            System.out.println("take raincoat for safety");
+        }
+    }
+}
