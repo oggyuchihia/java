063eefbb36e53854a2a8451a6b565763682e46b5
oggyuchihia <bhishmmori@gmail.com>
2026-06-01 17:56:36 +0530

    7wonders

---
+++ 7WONDERS.java
@@ -0,0 +1,45 @@
+class Wonder{
+    void.display(){
+        System.out.println("Wonders of world")
+    }
+}
+class Tajmahal extends Wonder{
+     void.display(){
+        System.out.println("Taj mahal agra")
+    }
+}
+class Greatwall extends Wonder{
+     void.display(){
+        System.out.println("Great wall of china")
+    }
+}
+class Machupichu extends Wonder{
+     void.display(){
+        System.out.println("Machu picchu Peru")
+    }
+}
+class Coloseumn extends Wonder{
+     void.display(){
+        System.out.println("Coloseumn rome italy")
+    }
+}
+class Piramid extends Wonder{
+     void.display(){
+        System.out.println("Piramid of giza egypt")
+    }
+}
+class Petra extends Wonder{
+     void.display(){
+        System.out.println("Petra jordan")
+    }
+}
+class Chichenitza extends Wonder{
+     void.display(){
+        System.out.println("Chichen itza mexico")
+    }
+}
+class Christtheredeemer extends Wonder{
+     void.display(){
+        System.out.println("Christ the redeemer peru")
+    }
+}
